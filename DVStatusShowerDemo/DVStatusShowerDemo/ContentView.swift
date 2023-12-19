fe72bfba91cf93579be0c6d276ba7dede451f721
Dhruvil-sim <125266321+Dhruvil-sim@users.noreply.github.com>
2023-12-19 11:50:22 +0530

    Initial Commit

---
+++ DVStatusShowerDemo/DVStatusShowerDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DVStatusShowerDemo
+//
+//  Created by Dhruvil Vora on 19/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DVStatusShowerDemo/DVStatusShowerDemo/DVStatusShowerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DVStatusShowerDemoApp.swift
+//  DVStatusShowerDemo
+//
+//  Created by Dhruvil Vora on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct DVStatusShowerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
